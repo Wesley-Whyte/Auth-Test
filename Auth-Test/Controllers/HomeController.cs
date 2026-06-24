034cadb282e8bac3be5d7157fe68df76bfe1b8dc
Wesley Emuobonuvie <wesoflife@yahoo.com>
2026-06-23 20:14:17 -0400

    Initial Commit

---
+++ Auth-Test/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Auth_Test.Models;
+
+namespace Auth_Test.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
